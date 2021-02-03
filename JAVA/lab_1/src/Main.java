b3a1be42ae548c5c716d5093d886fff3270255db
fairyteller <fairytellsblog@gmail.com>
2021-02-03 21:58:54 +0300

    Add new method

--- JAVA/lab_1/src/Main.java
+++ JAVA/lab_1/src/Main.java
@@ -19,6 +19,28 @@ class PartOne
 			System.out.print(" Не поместиться!");
 		}
 	}
+	static void markTranslator(int mark)
+	{
+		switch(mark)
+		{
+			case 1:
+				System.out.println("'Плохо'");
+				break;
+			case 2:
+				System.out.println("'Неудовлетворительно'");
+				break;
+			case 3:
+				System.out.println("'Удовлетворительно'");
+				break;
+			case 4:
+				System.out.println("'Хорошо'");
+				break;
+			case 5:
+				System.out.println("'Отлично'");
+				break;
+			}
+			
+	}
 }
 /*Составить программу, позволяющую получить
 словесное описание школьных отметок
@@ -38,6 +60,9 @@ class PartOne
 		System.out.print("Введите радиус: ");
 		double radius = input.nextDouble();
 		PartOne.isTriangleInCircle(radius,triangleLength);
-	    
+	
+		System.out.println("Введите оценку:");
+		int mark= input.nextInt();
+		PartOne.markTranslator(mark);    
 	}
 }
