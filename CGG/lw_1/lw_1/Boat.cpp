0765f74b7554ce66556737d4ce7cf254c2997302
Kurbatova <fairytellsblog@gmail.com>
2021-02-07 10:45:29 +0300

    change to UTF-8

--- CGG/lw_1/lw_1/Boat.cpp
+++ CGG/lw_1/lw_1/Boat.cpp
@@ -18,21 +18,21 @@ using namespace Gdiplus;
                                               processorArchitecture = '*' \
                                               publicKeyToken        = '6595b64144ccf1df' \
                                               language              = '*'\"")
-#pragma warning(disable : 4996) //отключает Ошибку deprecate. Возникает, когда используется устаревшая функция 
+#pragma warning(disable : 4996) //РѕС‚РєР»СЋС‡Р°РµС‚ РћС€РёР±РєСѓ deprecate. Р’РѕР·РЅРёРєР°РµС‚, РєРѕРіРґР° РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ СѓСЃС‚Р°СЂРµРІС€Р°СЏ С„СѓРЅРєС†РёСЏ 
 
-//Объявления
+//РћР±СЉСЏРІР»РµРЅРёСЏ
 
-HWND hwnd = NULL; //дескриптор окна
+HWND hwnd = NULL; //РґРµСЃРєСЂРёРїС‚РѕСЂ РѕРєРЅР°
 void Display(HDC hdc);
-/*Для изменения цвета окна, когда было обработано WM_SYSCHAR */
+/*Р”Р»СЏ РёР·РјРµРЅРµРЅРёСЏ С†РІРµС‚Р° РѕРєРЅР°, РєРѕРіРґР° Р±С‹Р»Рѕ РѕР±СЂР°Р±РѕС‚Р°РЅРѕ WM_SYSCHAR */
 RECT rc;
-HBRUSH brushes[3]; //кисти для изменения цвета окна
+HBRUSH brushes[3]; //РєРёСЃС‚Рё РґР»СЏ РёР·РјРµРЅРµРЅРёСЏ С†РІРµС‚Р° РѕРєРЅР°
 int brush_index = 0;
 
-/*Оконные процедуры*/
+/*РћРєРѕРЅРЅС‹Рµ РїСЂРѕС†РµРґСѓСЂС‹*/
 LRESULT CALLBACK MainWindowProcess(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-/*Обработчики WM_CREATE, WM_DESTROY, WM_SIZE*/
+/*РћР±СЂР°Р±РѕС‚С‡РёРєРё WM_CREATE, WM_DESTROY, WM_SIZE*/
 
 BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
 void OnDestroy(HWND hwnd);
@@ -40,20 +40,20 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int  CmdShow)
 { 
-	brushes[0] = (HBRUSH)CreateSolidBrush(RGB(0, 0, 100)); //Радикально-синий цвет
-	brushes[1] = (HBRUSH)CreateSolidBrush(RGB(0, 100, 0)); //Радикально-зеленый цвет
-	brushes[2] = (HBRUSH)CreateSolidBrush(RGB(100, 0, 0)); //Радикально-красный цвет
+	brushes[0] = (HBRUSH)CreateSolidBrush(RGB(0, 0, 100)); //Р Р°РґРёРєР°Р»СЊРЅРѕ-СЃРёРЅРёР№ С†РІРµС‚
+	brushes[1] = (HBRUSH)CreateSolidBrush(RGB(0, 100, 0)); //Р Р°РґРёРєР°Р»СЊРЅРѕ-Р·РµР»РµРЅС‹Р№ С†РІРµС‚
+	brushes[2] = (HBRUSH)CreateSolidBrush(RGB(100, 0, 0)); //Р Р°РґРёРєР°Р»СЊРЅРѕ-РєСЂР°СЃРЅС‹Р№ С†РІРµС‚
 
-	// регистрируем оконный класс главного окна...
+	// СЂРµРіРёСЃС‚СЂРёСЂСѓРµРј РѕРєРѕРЅРЅС‹Р№ РєР»Р°СЃСЃ РіР»Р°РІРЅРѕРіРѕ РѕРєРЅР°...
 	WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
 	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = MainWindowProcess; // оконная процедура
+	wcex.lpfnWndProc = MainWindowProcess; // РѕРєРѕРЅРЅР°СЏ РїСЂРѕС†РµРґСѓСЂР°
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1);
 
-	wcex.lpszClassName = TEXT("MainWindowProcess"); // имя класса
+	wcex.lpszClassName = TEXT("MainWindowProcess"); // РёРјСЏ РєР»Р°СЃСЃР°
 	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 	//wcex.hbrBackground = brushes[brush_index];
 
@@ -65,7 +65,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 
 	LoadLibrary(TEXT("ComCtl32.dll"));
 
-	/*Создание главного файла и обработка ошибки */
+	/*РЎРѕР·РґР°РЅРёРµ РіР»Р°РІРЅРѕРіРѕ С„Р°Р№Р»Р° Рё РѕР±СЂР°Р±РѕС‚РєР° РѕС€РёР±РєРё */
 	hwnd = CreateWindowEx(0, TEXT("MainWindowProcess"), TEXT("Chat"),
 		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 900, 600, NULL, NULL, hInstance, NULL);
 
@@ -75,9 +75,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	}
 	/*--------------------------------------------------*/
 
-	ShowWindow(hwnd, CmdShow); // отображаем главное окно
+	ShowWindow(hwnd, CmdShow); // РѕС‚РѕР±СЂР°Р¶Р°РµРј РіР»Р°РІРЅРѕРµ РѕРєРЅРѕ
 
-	/*Цикл обработки сообщений*/
+	/*Р¦РёРєР» РѕР±СЂР°Р±РѕС‚РєРё СЃРѕРѕР±С‰РµРЅРёР№*/
 	MSG  msg;
 	BOOL RetRes;
 	while ((RetRes = GetMessage(&msg, NULL, 0, 0)) != FALSE)
@@ -94,15 +94,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	}
 
 	return (int)msg.wParam;
-	//yjdsq rjvvtynfhb
 
 }
 
 #pragma region Work with Main Window
 LRESULT CALLBACK MainWindowProcess(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	HDC hdc;             // индекс контекста устройства
-	PAINTSTRUCT ps;      // структура для рисования
+	HDC hdc;             // РёРЅРґРµРєСЃ РєРѕРЅС‚РµРєСЃС‚Р° СѓСЃС‚СЂРѕР№СЃС‚РІР°
+	PAINTSTRUCT ps;      // СЃС‚СЂСѓРєС‚СѓСЂР° РґР»СЏ СЂРёСЃРѕРІР°РЅРёСЏ
 
 	switch (msg)
 	{
@@ -121,7 +120,7 @@ LRESULT CALLBACK MainWindowProcess(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lP
 			TCHAR szBuf[80];
 
 			hdc = GetDC(hwnd);
-			// Сохраняем координаты курсора мыши
+			// РЎРѕС…СЂР°РЅСЏРµРј РєРѕРѕСЂРґРёРЅР°С‚С‹ РєСѓСЂСЃРѕСЂР° РјС‹С€Рё
 			xPos = LOWORD(lParam);
 			yPos = HIWORD(lParam);
 			nSize = wsprintf(szBuf, TEXT("(%d, %d)"), xPos, yPos);
@@ -148,7 +147,7 @@ LRESULT CALLBACK MainWindowProcess(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lP
 
 	void OnDestroy(HWND hwnd)
 	{
-		PostQuitMessage(0); // отправляем сообщение WM_QUIT
+		PostQuitMessage(0); // РѕС‚РїСЂР°РІР»СЏРµРј СЃРѕРѕР±С‰РµРЅРёРµ WM_QUIT
 	}
 	
 	void Display(HDC hdc)
@@ -164,9 +163,9 @@ LRESULT CALLBACK MainWindowProcess(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lP
 
 		//temp.Clear(Color::Aqua);
 		//SolidBrush brush(Color::Black);
-		//Pen penBrown(&brush,6.f);//Для рисования контура
+		//Pen penBrown(&brush,6.f);//Р”Р»СЏ СЂРёСЃРѕРІР°РЅРёСЏ РєРѕРЅС‚СѓСЂР°
 		//
-		////Для первого прямоугольника
+		////Р”Р»СЏ РїРµСЂРІРѕРіРѕ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°
 		//Point pt1(10,10);
 		//Point pt2(300,300);
 		//temp.FillRectangle(&brush, 320, 330, 500, 70);
