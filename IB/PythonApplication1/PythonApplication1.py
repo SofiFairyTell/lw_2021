fa3a6ada1ff418425b13ab05d24be0a593ee643b
Kurbatova <fairytellsblog@gmail.com>
2021-02-09 01:08:16 +0300

    Final
    Готовый код, сделан исполняемый файл

--- IB/PythonApplication1/PythonApplication1.py
+++ IB/PythonApplication1/PythonApplication1.py
@@ -2,9 +2,17 @@ import itertools
 import string
 import sys
 
+#Важная информация! По ссылке объяснение как .py файлы компилировать в .exe
+#http://nikovit.ru/blog/samyy-prostoy-sposob-skompilirovat-python-fayl-v-exe/
+
 #глобальные переменные
 alphabet='абвгдеёжзийклмнопрстуфхцчшщъьыэюя_,.'
 
+#шифр Плейфера
+#шаг 1: составление матрицы. Входные данные : алфавит и ключевое слово
+#Первая строка заполняется содержимым ключевого слова. 
+#Символы не должны повторяться. 
+#Остальная часть таблицы - по порядку те буквы алфавита, которых нет в слове. 
 def chunker(seq, size):
     it = iter(seq)
     while True:
@@ -12,7 +20,6 @@ def chunker(seq, size):
         if not chunk:
             return
         yield chunk
-
 
 def my_input(plaintext: str) -> str:
     
@@ -35,14 +42,7 @@ def my_input(plaintext: str) -> str:
     if len(plaintext_return) & 1:
         plaintext_return += "ё"
 
-    #print("БИГРАММЫ:{}".format(plaintext_return))
     return plaintext_return
-
-#шифр Плейфера
-#шаг 1: составление матрицы. Входные данные : алфавит и ключевое слово
-#Первая строка заполняется содержимым ключевого слова. 
-#Символы не должны повторяться. 
-#Остальная часть таблицы - по порядку те буквы алфавита, которых нет в слове. 
 
 def generate_matrix(key:str)->[str] :
     matrix = [] 
@@ -82,8 +82,6 @@ def playfair_encrypt(plaintext:str, key:str)->str:
 
     return ciphertext
 
-
-
 def playfair_decode(ciphertext: str, key: str) -> str:
     table = generate_matrix(key)
     plaintext = ""
@@ -120,6 +118,9 @@ def cesar_encrypt(input_str:str, shift:int)->str:
 
 	return encrypted_message
 
+def cesar_decrypt(input_string: str, key: int) -> str:
+    key *= -1
+    return cesar_encrypt(input_string, key)
 
 #Мультипликативный моноалфавитный шрифт с задаваемым смещением (Афинный шифр)
 
@@ -151,12 +152,12 @@ def findModInverse(a: int, m: int) -> int:
 
 
 #Здесь проверяем правильность введенного ключа
-def check_keys(keyA, keyB, mode)-> None:
+def check_keys(keyA, keyB, alphabet, mode)-> None:
 	if mode == 'encrypt' and keyA == 1 or keyB == 0: sys.exit("Ключ слаб для шифрования. Выберите другой ключ")
 	#if mode == 'encrypt'  and : sys.exit('Ключ слаб для шифрования. Выберите другой ключ') 
 	if keyA < 0 or keyB < 0 or keyB > len(alphabet) - 1: 
 	      sys.exit("Первая часть ключа должна быть >0 и вторая часть ключа должна быть от 0 до {(len(alphabet)-1)}")
-	if gcd(keyA,len(alphabet)) != 1: 
+	if gcd(keyB, len(alphabet)) != 1: 
 		  sys.exit('Первая часть ключа и число символов в алфавите не взаимно простые.Выбрать другой ключ')		
 	else: print("Проверка пройдена")
 
@@ -170,67 +171,88 @@ def divide_key(key):
 #Афинный шифр
 #Зашифруем сообщение
 def affin_encrypt(input_str:str, key:int)->str:
-	
-	encrypted_message = ''#инициализация строки для сообщения
-
-	keyA, keyB = divide_key(key) #выполним разделение нашего ключа на две части
-	check_keys(keyA, keyB, 'encrypt') #проверим ключ
-	
-	for char in input_str:
-		if char not in alphabet:
-			encrypted_message += char #символа нет в алфавите, добавим незашифрованным
-		else:
-			index = alphabet.find(char)
-			encrypted_message += alphabet[(index*keyA + keyB) % len(alphabet)]	#Добавим закодированный символ в сообщение
-
-	return encrypted_message
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъьыэюя"
+    encrypted_message = ''#инициализация строки для сообщения
+    keyA, keyB = divide_key(key) #выполним разделение нашего ключа на две части
+    check_keys(keyA, keyB, alphabet,'encrypt') #проверим ключ
+    for char in input_str:
+       if char not in alphabet:
+          encrypted_message += char #символа нет в алфавите, добавим незашифрованным
+       else:
+            index = alphabet.find(char)
+            encrypted_message += alphabet[(index*keyB + keyA) % len(alphabet)]	#Добавим закодированный символ в сообщение
+    return encrypted_message
 
 #Расшифруем сообщение
 def affin_decrypt(input_str:str, key:int)->str:
-	
-	decrypted_message = '' #инициализация строки для сообщения 
-	keyA, keyB = divide_key(key) #выполним разделение нашего ключа на две части
-	check_keys(keyA, keyB,'decrypt') #проверим ключ
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъьыэюя"	
+    decrypted_message = '' #инициализация строки для сообщения 
+    keyA, keyB = divide_key(key) #выполним разделение нашего ключа на две части
+    check_keys(keyA, keyB, alphabet, 'decrypt') #проверим ключ
+    inverse_keyB = findModInverse(keyB, len(alphabet)) #получим мультипликативную инверсию первой части ключа
 
-	inverse_keyA = findModInverse(keyA, len(alphabet)) #получим мультипликативную инверсию первой части ключа
-
-	for char in input_str:
-		if char not in alphabet:
-			decrypted_message += char
-		else:
-			index = alphabet.find(char)
+    for char in input_str:
+        if char not in alphabet:
+            decrypted_message += char
+        else:
+            index = alphabet.find(char)
 			#Добавим закодированный символ 
-			decrypted_message += alphabet[( index - keyB)*inverse_keyA % len(alphabet)]
+            decrypted_message += alphabet[( index - keyA)*inverse_keyB % len(alphabet)]
 
-	return decrypted_message
+    return decrypted_message
+
+def char_frequancy(txt:str)->None:
+   b = {}
+   mess_len = 0
+   #with open('test2.txt',"r") as f:
+   for l in txt:
+        if (l in alphabet):
+                mess_len +=1
+                if (l in b):
+                    b[l] +=1
+                else:
+                    b[l] = 1
+   #for u in b.keys():
+   print('В тексте длиной: {} Частота встречаемоти сообщения {} '.format(mess_len, b.items()))
+   #for u in b.keys():
+   # print('Символ {} встречается с частотой {: .1f} '.format(u,(b[u]/mess_len)))
+
+
+
+
+
+
 
 #main
 print("Начало выполнения программы")
-
-#str = input("Сообщение: ")
-#key = int(input("Ключ: "))
-
 while True:
-    mess = input("Сообщение: ")
-    cypher_method = int(input("Введите номер метода шифрования:"))
-    if cypher_method == 1: 
+    mess = input("Сообщение:  ")
+    cypher_method = int(input("Введите номер метода шифрования:  "))
+    if cypher_method == 1:
+       print("\t\vШифр Цезаря:")
        key = int(input("Ключ: "))
        result = cesar_encrypt(mess,key)
-       print("Ваше зашифрованное сообщение:{}".format(result))
+       print("Ваше зашифрованное сообщение:  {}".format(result))
+       char_frequancy(result)
+       result = cesar_decrypt(result,key)
+       #print("Ваше исходное сообщение:  {}".format(result))
        #break
     elif cypher_method == 2:
-       key = int(input("Ключ: "))
+       print("\t\vАфинный шифр")
+       key = int(input("Ключ:  "))
        result = affin_encrypt(mess,key) #тест на key = 1334 проходит хорошо
-       print("Ваше зашифрованное сообщение:{}".format(result))
+       print("Ваше зашифрованное сообщение:  {}".format(result))
+       char_frequancy(result)
        result = affin_decrypt(result,key)
-       print("Ваше исходное сообщение:{}".format(result))
+       print("Ваше исходное сообщение:  {}".format(result))
        #break
     elif cypher_method == 3:
-       key_phrase = input("Ключевая фраза:")
+       print("\t\v Шифр Плейфера:")
+       key_phrase = input("Ключевая фраза:  ")
        result = playfair_encrypt(mess,key_phrase)
-       print("Ваше зашифрованое сообщение:{}".format(result))
+       print("Ваше зашифрованое сообщение:  {}".format(result))
        result = playfair_decode(result, key_phrase)
-       print("Ваше исходное сообщение:{}".format(result))
+       print("Ваше исходное сообщение:  {}".format(result))
 
     else: break
 
