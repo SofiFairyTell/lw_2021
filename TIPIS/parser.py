f4df1c10b5a15fc10a61efd35733c35abfea8184
Kurbatova <fairytellsblog@gmail.com>
2021-02-16 21:44:44 +0300

    Add parser and entropy calculate method
    Добавлен простейший, но оптимизированный модуль для обработки текста (удаление заданных символов, перевод все в нижний регистр), и модуль для расчета энтропии при условии независимости символов

---
+++ TIPIS/parser.py
@@ -0,0 +1,37 @@
+import string
+import sys
+import itertools
+import re
+# имя файла
+FILENAME = "C:\\Users\\Kurbatova\\source\\LW2020\\lw_2021\\TIPIS\esperanto_full.txt"
+FILENAME2 = "C:\\Users\\Kurbatova\\source\\LW2020\\lw_2021\\TIPIS\esperanto_stemm.txt"
+
+# определяем пустой список
+messages = list()
+ 
+##for i in range(4):
+##    message = input("Введите строку " + str(i+1) + ": ")
+##    messages.append(message + "\n")
+## 
+### запись списка в файл
+##with open(FILENAME, "a") as file:
+##    for message in messages:
+##        file.write(message)
+ 
+### считываем сообщения из файла
+##print("Считанные сообщения")
+##with open(FILENAME, "r") as file:
+##    for message in file:
+##        print(message, end="")
+symb = [',','.',' ',]
+#list = ['b', 'a', 'c', 'd','e','f',' ','l','m','n','h','i','j','k','t','r','w','u']
+with open(FILENAME, "r") as file:
+    with open(FILENAME2,"a") as file2:
+        line = file.readline()
+        while line:
+            mess = line.lower()
+            message = mess.translate({ord(x): '' for x in symb})
+            #message = line.replace("f","")
+            file2.write(message)
+            print(message, end="")
+            line = file.readline()
