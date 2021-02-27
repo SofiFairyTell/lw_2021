18fe25c3fa98a8b7cf2e5712829daf9d95c5d353
Kurbatova <fairytellsblog@gmail.com>
2021-02-28 00:08:12 +0300

    first app
    - опробован функционал работы с контроллерами
    - собран проект

--- JAVA/lw_1/GUIapp/src/sample/Controller.java
+++ JAVA/lw_1/GUIapp/src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Polygon;
 
-public class Controller {
+
+public class Controller
+{
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button btn;
+    @FXML
+    private void click(ActionEvent event)
+    {
+        btn.setText("You've clicked!");
+        String text = textField.getText();
+        textArea.setText(text);
+
+    }
 }
--- JAVA/lw_1/GUIapp/src/sample/Main.java
+++ JAVA/lw_1/GUIapp/src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("g.fxml"));
         primaryStage.setTitle("LW_1");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
     }
 
@@ -20,4 +20,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
