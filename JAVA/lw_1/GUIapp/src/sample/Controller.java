47dd591235aee16895e2e49ad5d084db8e2d2edc
Kurbatova <fairytellsblog@gmail.com>
2021-02-28 16:56:01 +0300

    start 3 task
    - намечена структура графики в 3-й задаче
    (?) как передать двумерный массив в TableView

--- JAVA/lw_1/GUIapp/src/sample/Controller.java
+++ JAVA/lw_1/GUIapp/src/sample/Controller.java
@@ -1,16 +1,35 @@
 package sample;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Polygon;
 import ru.bstu.it32.kurbatova.lab1.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Controller
 {
+
+    @FXML
+    private TableView<ObservableList<Double>> table;
+    @FXML
+    private TableColumn<Double,Double> column;
+    @FXML
+    private  TableColumn<Double,Double> column1;
+    @FXML
+    private TextField start;
+    @FXML
+    private TextField end;
+    @FXML
+    private TextField step;
     @FXML
     private TextArea result_mark;
     @FXML
@@ -23,6 +42,8 @@ public class Controller
     private TextArea result_TrInCir;
     @FXML
     private Button btn;
+    private Button btn_mark;
+    public Button btn_tri;
 
     @FXML
     public void initialize(){
@@ -45,5 +66,22 @@ public class Controller
     {
         int mark_i = Integer.parseInt(mark.getText());
         result_mark.setText(ComputeMethod.markTranslator(mark_i));
+    }
+    @FXML
+    private void TrigonometryFunction_click(ActionEvent actionEvent)
+    {
+        int start_i = Integer.parseInt(start.getText());
+        int end_i = Integer.parseInt(end.getText());
+        int step_i = Integer.parseInt(step.getText());
+        double[][] array = ComputeMethod.TrigonometryFunction(start_i,end_i,step_i);
+
+    ObservableList<Double>
+        TableColumn x = new TableColumn("X");
+        TableColumn y = new TableColumn("Y");
+        table.getColumns().addAll(x,y);
+        table.setItems();
+
+
+
     }
 }
