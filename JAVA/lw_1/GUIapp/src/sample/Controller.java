b2647476fba610c2ecfc1ad1a24ffae20b925bb5
Kurbatova <fairytellsblog@gmail.com>
2021-02-28 13:39:22 +0300

    finis first task
    - исправлена работа кнопок
    - проверено на правильность обработки

--- JAVA/lw_1/GUIapp/src/sample/Controller.java
+++ JAVA/lw_1/GUIapp/src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Polygon;
 import ru.bstu.it32.kurbatova.lab1.*;
 
@@ -14,9 +16,19 @@ public class Controller
     @FXML
     private TextField length;
     @FXML
-    private TextArea textArea;
+    private TextArea result;
     @FXML
     private Button btn;
+
+    @FXML
+    public void initialize(){
+        btn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                btn.setText("Thanks!");
+            }
+        });
+    }
     @FXML
     private void click(ActionEvent event)
     {
@@ -25,7 +37,6 @@ public class Controller
         int rad = Integer.parseInt(radius.getText());
         ComputeMethod t;
         t = new ComputeMethod();
-        textArea.setText(ComputeMethod.isTriangleInCircle(len,rad));
-
+        result.setText(ComputeMethod.isTriangleInCircle(rad,len));
     }
 }
