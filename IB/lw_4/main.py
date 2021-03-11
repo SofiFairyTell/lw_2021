124540c87696fdd878b6c991683be3b65d1eb9ab
Kurbatova <fairytellsblog@gmail.com>
2021-03-11 10:11:26 +0300

    add Rabin-Miller test
    - добавлен метод с тестом Рабина-Миллера
    (!) не тестировалось

--- IB/lw_4/main.py
+++ IB/lw_4/main.py
@@ -4,6 +4,8 @@
 import math as math
 import random as random
 
+from past.builtins import xrange
+
 
 class TestForNumbers:
     number = int()
@@ -26,18 +28,49 @@ class TestForNumbers:
             3. Вывод:
                 1.  r = 1, number - простое
                 2. r != 1 , number - составное"""
-        a = random.randint(2, number-2)
-        r = int((a**(number - 1)) % number)
+        a = random.randint(2, number - 2)
+        r = int((a ** (number - 1)) % number)
         if r == 1:
             print("Число {0} простое".format(number))
         else:
             print("Число {0} составное".format(number))
         # Todo
 
+    def toBinary(self, n):
+        r = []
+        while n > 0:
+            r.append(n % 2)
+            n = n / 2
+            return r
+
     def RabinMiller_test(self, number, test_step):
-        if (number):
-            print("Задано количество проверок")
-            # Todo
+        # Входные данные: число number >=5, к-во испытаний - test_step
+        # Выходные данные: простое/составное
+        # Алгоритм
+        """
+            1. Представить number - 1 = 2^s r , где r - нечетное
+            2. Выбрать случайное целое 2<= a< = number -2
+            3. y = a^r(mod n)
+            4. y != 1
+                1. j = 1,
+                2. если j<=s-1 и y != number-1, то y = y^2(mod n)
+            4. Вывод:
+                1.  y = 1 , number - простое, j = j+1
+                2. y != number-1 , number - составное"""
+        for j in xrange(1, test_step + 1):
+            a = random.randint(1, number - 1)
+            b = self.toBinary(number - 1)
+            d = 1
+            for i in xrange(len(b) - 1, -1, -1):
+                x = d
+                d = (d * d) % number
+                if d == 1 and x != 1 and x != number - 1:
+                    return True  # Составное
+                if b[i] == 1:
+                    d = (d * a) % number
+                    if d != 1:
+                        return True  # Составное
+                    return False  # Простое
 
     def Partibility_test(self, number) -> str:
         time = ""
