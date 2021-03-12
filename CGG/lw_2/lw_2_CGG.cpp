b658a9f7fc9968f47a7a114a123ccfab2ed9f75e
Kurbatova <fairytellsblog@gmail.com>
2021-03-12 15:08:17 +0300

    add vector computator
    - добавлен блок для вычисления координат векторов
    - добавлено вычисление углов векторов
    - добавлено условие на проверку их суммы
    - не тестировалось
    
    Co-Authored-By: GeorgeAndzhich <72293273+GeorgeAndzhich@users.noreply.github.com>

--- CGG/lw_2/lw_2_CGG.cpp
+++ CGG/lw_2/lw_2_CGG.cpp
@@ -54,6 +54,7 @@ string Triangle_method(Dot& A, Dot& B, Dot& C, Dot& P )
 	// Вычисление: 
 		//cos α = 	(a·b)/ |a|·|b| ~ 0,1 or 0,96 -> arcos a ~ 30` 
 		//у нас для angle APC вектор a = P.x - A.x
+		// Пример расчета: APB =arcos( (-0.4*0.6 + 4.3*-1.6) / (1.71 * 4.32 ) )= arcos((-7.12 / 7.37)) = 165*93`
 	// Условия завершения: 
 	// Сумма углов: 
 		//  = 360 - точка внутри и на границе
@@ -69,12 +70,33 @@ string Triangle_method(Dot& A, Dot& B, Dot& C, Dot& P )
 	}
 	else
 	{
-		// векторы
+		//координаты векторов
+		// Может не точки?? 
+		Dot vector_pa = Dot();
+		Dot vector_pb = Dot();
+		Dot vector_pc = Dot();
+
+		vector_pa.setCoordinates(P.X() - A.X(), P.Y() -A.Y());
+		vector_pb.setCoordinates(P.X() - B.X(), P.Y() - B.Y());
+		vector_pc.setCoordinates(P.X() - C.X(), P.Y() - C.Y());
+	
+		// векторы длины
 		float pa = sqrt(pow((P.X() - A.X()),2)+pow(P.Y() - A.Y(),2));
 		float pb = sqrt(pow((P.X() - B.X()),2)+pow(P.Y() - B.Y(),2));
 		float pc = sqrt(pow((P.X() - C.X()),2)+pow(P.Y() - C.Y(),2));
-		// углы?
-		
+		// углы
+		float angle_apb = (acos((vector_pa.X()*vector_pb.X()+vector_pa.Y()*vector_pb.Y())/(pa*pb)) *180)/ PI ; //в градусах
+		float angle_bpc = (acos((vector_pb.X()*vector_pc.X()+vector_pb.Y()*vector_pc.Y())/(pb*pc)) *180)/ PI ; //в градусах
+		float angle_apc = (acos((vector_pa.X()*vector_pc.X()+vector_pa.Y()*vector_pc.Y())/(pa*pc)) *180)/ PI ; //в градусах
+		// сумма углов
+		float sum_angle = angle_apb + angle_bpc + angle_apc;
+		if (sum_angle != 360)
+		{
+			cout << "Точка вне обозначенной области"
+		}
+		else 
+			cout << "Точка внутри обозначенной области"
+
 	}
 
 };
