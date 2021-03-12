487b1af5790bcb2a39e540b9e5b21e08d9bc09d1
Kurbatova <fairytellsblog@gmail.com>
2021-03-12 14:47:25 +0300

    add check point method
    - описаны формулы для расчетов
    - добавлен метод проверки точки на совпадение с вершиной
    
    Co-Authored-By: GeorgeAndzhich <72293273+GeorgeAndzhich@users.noreply.github.com>

--- CGG/lw_2/lw_2_CGG.cpp
+++ CGG/lw_2/lw_2_CGG.cpp
@@ -1,34 +1,102 @@
 #include <iostream>
+#include<cmath>
+
 
 using namespace std;
-
-/*TODO
-1. Создать точки треугольника ABC с координамами в виде x,y
-2. Обеспечить ввод точки пользователем.
-
-*/
-
-
+// To do 
+//Определить принадлежность через Треугольники
+// Определеить принадлежность через Барицентрические координаты
 class Dot {
 private:
 	int x;
 	int y;
 public:
-    //Getters for X and Y
+	//getters
 	int X() { return x; }
 	int Y() { return y; }
+	//setters
+	void setX(int value) { x = value; }
+	void setY(int value) { y = value; }
 
-    //Setters for X and Y
-	void setX(int value){x = value;}
-    void setY(int value){y = value;}     
-    int SetCoords()
-    {
-        return 0;
-    }
+	void setCoordinates(Dot &dot)
+	{
+		int value1, value2;
+		cout << "Enter the coordinates: \n";
+		cin >> value1; cout << "\t"; cin >> value2;
+		dot.setX(value1); dot.setY(value2);
+	}
+	
+	
 };
+
+bool EqualDot(Dot &first, Dot &second)
+{
+	string answer;
+	if (first.X() == second.X() && first.Y() == second.Y())
+	{
+		return true;
+		cout << first.X() << first.Y() << "\t" << second.X() << second.Y();
+	}
+	else
+		return false;
+}
+
+string Triangle_method(Dot& A, Dot& B, Dot& C, Dot& P )
+{
+	string answer;
+	//внутри, снаружи, на границе
+	// P - ее местоположение нужно определить
+	//  A, B,C - координаты вершин
+	// Формула: a*b = |a||b| cos y , 
+	// где a и b - векторы , y - угол между ними
+	// Вектора: AB, BC, CA - для треугольника
+	// |AB| = sqrt((B.x - A.x)^2 + (B.y - A.y)^2 )
+	// Вычисление: 
+		//cos α = 	(a·b)/ |a|·|b| ~ 0,1 or 0,96 -> arcos a ~ 30` 
+		//у нас для angle APC вектор a = P.x - A.x
+	// Условия завершения: 
+	// Сумма углов: 
+		//  = 360 - точка внутри и на границе
+		// != 360 - вне треугольника
+	// Углы: 
+		// = 180 - точка на границе
+	// Совпадение координат 
+		// Если P.x = A.x и P.y = A.y, то P - совпадает с A, 
+		// answer = границе треугольника
+	if (EqualDot(P,B) == true || EqualDot(P,A) = true || EqualDot(P,C))
+	{
+		cout<<"Точка совпала с одной из вершин"<<endl;
+	}
+	else
+	{
+		// векторы
+		float pa = sqrt(pow((P.X() - A.X()),2)+pow(P.Y() - A.Y(),2));
+		float pb = sqrt(pow((P.X() - B.X()),2)+pow(P.Y() - B.Y(),2));
+		float pc = sqrt(pow((P.X() - C.X()),2)+pow(P.Y() - C.Y(),2));
+		// углы?
+		
+	}
+
+};
+string Baricenter_menthod()
+{
+	string answer;
+	return answer;
+}
+
+
 
 int main()
 {
-
+	cout << "---------------START--------------------"; 
+	cout << "Enter the coordinates for the following dots:\n";
+	Dot A = Dot();
+	Dot B = Dot();
+	Dot C = Dot();
+	Dot P = Dot(); // Точка которую будем проверять
+	cout << "A";
+	cout << "B";
+	cout << "C";
+	cout << "P";
 	return 0;
 }
