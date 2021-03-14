71b6c67733795ac5b937b6b5050f9593c0dbc983
Kurbatova <fairytellsblog@gmail.com>
2021-03-14 22:05:50 +0300

    add countdown
    - добавлен скрипт для обратного отсчета
    - протестировано

--- TCS/LAB_1/TRAVEL_WEB/js/lifehack.js
+++ TCS/LAB_1/TRAVEL_WEB/js/lifehack.js
@@ -11,32 +11,7 @@ $(document).ready(function(){
 			$('div.carouselNext, div.carouselPrev').removeClass('visible');
 	});	
     
-	// Flip card to the back side
-	$('.view_details').click(function(){		
-		$('div.carouselNext, div.carouselPrev').removeClass('visible');
-		$('.product-card').addClass('flip-10');
-		setTimeout(function(){
-			$('.product-card').removeClass('flip-10').addClass('flip90').find('div.shadow').show().fadeTo( 80 , 1, function(){
-				$('.product-front, .product-front div.shadow').hide();			
-			});
-		}, 50);
-		
-		setTimeout(function(){
-			$('.product-card').removeClass('flip90').addClass('flip190');
-			$('.product-back').show().find('div.shadow').show().fadeTo( 90 , 0);
-			setTimeout(function(){				
-				$('.product-card').removeClass('flip190').addClass('flip180').find('div.shadow').hide();						
-				setTimeout(function(){
-					$('.product-card').css('transition', '100ms ease-out');			
-					$('.cx, .cy').addClass('s1');
-					setTimeout(function(){$('.cx, .cy').addClass('s2');}, 100);
-					setTimeout(function(){$('.cx, .cy').addClass('s3');}, 200);				
-					$('div.carouselNext, div.carouselPrev').addClass('visible');				
-				}, 100);
-			}, 100);			
-		}, 150);			
-	});			
-    
+
 
 	// Flip card back to the front side
 	$('.flip-back').click(function(){		
@@ -102,3 +77,29 @@ $(document).ready(function(){
 	});
   
 });
+	// Flip card to the back side
+	$('.view_details').click(function(){		
+		$('div.carouselNext, div.carouselPrev').removeClass('visible');
+		$('.product-card').addClass('flip-10');
+		setTimeout(function(){
+			$('.product-card').removeClass('flip-10').addClass('flip90').find('div.shadow').show().fadeTo( 80 , 1, function(){
+				$('.product-front, .product-front div.shadow').hide();			
+			});
+		}, 50);
+		
+		setTimeout(function(){
+			$('.product-card').removeClass('flip90').addClass('flip190');
+			$('.product-back').show().find('div.shadow').show().fadeTo( 90 , 0);
+			setTimeout(function(){				
+				$('.product-card').removeClass('flip190').addClass('flip180').find('div.shadow').hide();						
+				setTimeout(function(){
+					$('.product-card').css('transition', '100ms ease-out');			
+					$('.cx, .cy').addClass('s1');
+					setTimeout(function(){$('.cx, .cy').addClass('s2');}, 100);
+					setTimeout(function(){$('.cx, .cy').addClass('s3');}, 200);				
+					$('div.carouselNext, div.carouselPrev').addClass('visible');				
+				}, 100);
+			}, 100);			
+		}, 150);			
+	});			
+    
