f67d04618f515317bd4a17d235ee10c117484ac7
Kurbatova <fairytellsblog@gmail.com>
2021-03-26 15:04:35 +0300

    use tuple
    - удален лишний код
    - оптимизировано до bool-значений
    - вывод через tuple
    
    Co-Authored-By: GeorgeAndzhich <72293273+GeorgeAndzhich@users.noreply.github.com>

--- CGG/lw_2/lw_2_CGG.cpp
+++ CGG/lw_2/lw_2_CGG.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <gdiplus.h>
 #include <gdiplusgraphics.h>
+#include <tuple>
 
 #define PI 3.1415
 #pragma comment(lib, "GdiPlus.lib")
@@ -98,33 +99,15 @@ class EqualMethod
 		{
 			return (abs(a) == tolerance);
 		}
+
+		
 };
 
 
 class BelongPolygon
 {
 	public:
-		/*		
-			Точка P - ее местоположение нужно определить: внутри, снаружи, на границе
-			A, B,C - координаты вершин 
-			Формула: a*b = |a||b| cos y , 
-			где a и b - векторы , y - угол между ними
-			Вектора: AB, BC, CA - для треугольника
-				|AB| = sqrt((B.x - A.x)^2 + (B.y - A.y)^2 )
-			Вычисление: 
-				cos α = 	(a·b)/ |a|·|b| ~ 0,1 or 0,96 -> arcos a ~ 30` 
-			у нас для angle APC вектор a = P.x - A.x
-				Пример расчета: APB =arcos( (-0.4*0.6 + 4.3*-1.6) / (1.71 * 4.32 ) )= arcos((-7.12 / 7.37)) = 165*93`
-			Условия завершения: 
-			Сумма углов: 
-				= 360 - точка внутри и на границе
-				!= 360 - вне треугольника
-			Углы: 
-				= 180 - точка на границе
-			Совпадение координат 
-				Если P.x = A.x и P.y = A.y, то P - совпадает с A, 
-				answer = границе треугольника
-		*/
+		
 
 		string Triangle_method(Dot& A, Dot& B, Dot& C, Dot& P)
 		{
@@ -158,21 +141,7 @@ class BelongPolygon
 					return "Dot is in the bounds";
 			}
 		};
-		/*
-			P = aA + b + cC where  (a,b,c) - барицентрические координаты
-			P.y = aA + bB + cC where  (a,b,c) - барицентрические координаты
-			(a+b+c=1) and a >=0 and b >=0 and c >=0 (*)
-			Выходные данные:
-				Если * верно и хотя бы одна координат (a, b,c) = 0, то на границе
-				Если * верно и хотя бы одна координат (a, b,c) = 1, то совпадение с вершиной
-				Если * не верно, то вне треугольника
-			denominate = (By - Cy)(Ax - Cx) + (Cx - Bx)(Ay - Cy)
-			a = (By - Cy)(Px-Cx) + (Cx-Bx)(Py-Cy) / denominate; 
-			b = (Cy - Ay)(Px - Cx) + (Ax - Cx)(Py - Cy)/ denominate; 
-			c = (Ay - By)(Px - Ax) + (Bx -  Ax)(Py - Ay)/ denominate;
-		*/
-
-
+		
 		string Baricenter_method(Dot& A, Dot& B, Dot& C, Dot& P)
 		{
 			string answer;
@@ -208,34 +177,45 @@ class BelongPolygon
 			return answer;
 		}
 
-		string Baricenter_method(PointF& A, PointF& B, PointF& C, PointF& P)
+		tuple<const bool, int> Baricenter_method(PointF& A, PointF& B, PointF& C, PointF& P, int check_border)
 		{
 			string answer;
 			EqualMethod Equal = EqualMethod();
+			if (Equal.EqualDot(A, P) || Equal.EqualDot(B, P)|| Equal.EqualDot(C, P))
+			{
+				cout << "Dot is equal to this height"; //местоположение в многоугольнике
+				cout << "Dot belong Polygon";
+				tuple <const bool, int> answer = { true, check_border};
+				return answer; // = "Dot belong Polygon"; //вывод - TRUE?
+			}
+			
 			float denominate = ((B.Y - C.Y) * (A.X - C.X)) + ((C.X - B.X) * (A.Y - C.Y));
 			float a = (((B.Y - C.Y) * (P.X - C.X)) + ((C.X - B.X) * (P.Y - C.Y))) / denominate;
 			float b = (((C.Y - A.Y) * (P.X - C.X)) + ((A.X - C.X) * (P.Y - C.Y))) / denominate;
 			float c = (((A.Y - B.Y) * (P.X - C.X) + ((B.X - A.X) * (P.Y - A.Y)))) / denominate;
 
-			//Не проверять check_summ
-			//int check_sum = (abs(a) + abs(b) + abs(c)); //без abs вернет 0, т.к. отрицательные значения в весах
-			//float check_sum = ( a+b+c); //без abs вернет 0, т.к. отрицательные значения в весах
-
-			/*if (check_sum == 1 && (abs(a) >= 0 && abs(b) >= 0 && abs(c) >= 0))*/
 			if (Equal.GreaterOrEqual(a,0) && Equal.GreaterOrEqual(b, 0)&& Equal.GreaterOrEqual(c, 0))
 			{
-				answer = "The dot is in the triangle";
+				cout <<"The dot is in the triangle";
 				// уточнение, т.е. где именно она в треугольнике
 				if (OnBorder(a,b,c))
 				{
-					return answer = "The dot is on the border";
+					cout << "The dot is on the border";
+					check_border++; 
+					tuple <const bool, int> t = {true, check_border};
+					return t; 				
+				}
+				else
+				{
+					tuple <const bool, int> answer = {true, check_border };
+					return answer; //принадлежит
 				}			
-				return answer;
 			}
 			else
-				answer = "\nGiven dot is not in the triangle";
-
-			return answer;
+			{
+				tuple <const bool, int> answer = {false, check_border };
+				return answer;//Given dot is not in the triangle";
+			}
 		}
 
 		bool OnBorder(float a, float b, float c)
@@ -252,31 +232,7 @@ class BelongPolygon
 class Method
 {
 public:
-	/**
-	* inPolygonTrue = true; для проверки при совпадении на границе
-	* Следовательно: если из какой-нибудь вершины многоугольника провести все возможные диагонали,
-	* то они разделят многоугольник на треугольники: t=n-2
-	* ФОРМУЛА поиска количества треугольников: triangle = n-2 -> n=5 triangle = 3 , n=7 triangle = 5.
-	* Входные данные: точки, количество вершин - n
-	* В качестве первой точки возьмем первую в списке. Далее по очереди будем брать следующие.
-	* Пример:
-		n = number_vertex; - количество вершин
-		for(i = 1; i<number_vertex++ ; number_vertex++)
-			triangle_list(A, i, i+1);
-		т.е. для первого  треугольника 0, 1, 2 номера вершин
-			 для второго  треугольника 0, 2, 3
-			 для третьего треугольника 0, 3, 4
-			 ....
-			if (answer = "The dot is on the border")
-			{
-				//продолжить проверку
-			}			
-			
-			Last triangle ADE startDot = 0, dot[3], dot[4] 
-			startDot = 0, A, dots[0] = B, dots[1]= C : i = 0
-			startDot = 0 = A, dots[2] = C, dots[i+1] = D : i = 1
-			startDot = 0 = A , dots[3] = D , dots[i+1] = E : i = 2
-	*/
+	
 	string Polygon_method(vector<Dot> dots, Dot& P)
 	{
 		string answer;
@@ -344,65 +300,90 @@ public:
 	{
 		string answer;
 		EqualMethod Equal = EqualMethod();
+
 		// Проверка на совпадение с вершиной
 		for (auto &elem : dots)
 		{
 			if (Equal.EqualDot(elem, P) == true)
 			{
-				return answer = "Dot is equal to this height";
 				cout << elem.X << '\t' << elem.Y;
+				return answer = "Dot is equal to this height"; //На самом деле - принадлежит многогольнику
 				break;
 			}
 		}
 		BelongPolygon inPolygon = BelongPolygon();
 		int check_border = 0;
+		int check;
+		bool answ;
 		PointF startDot = PointF();
 		startDot = dots.front();
 		dots.erase(dots.begin());
 		for (int i = 0; i < dots.size() - 1; ++i)
 		{
-			cout << "WORK WITH TRIANGLE: " << i+1;
-			answer = inPolygon.Baricenter_method(startDot, dots[i], dots[i + 1], P);
-			cout << endl << answer << endl;
-			if (answer.compare("\nGiven dot in the triangle") == 0 && check_border == 1)
+			cout << "WORK WITH TRIANGLE: " << i + 1;
+			tie(answ, check) = inPolygon.Baricenter_method(startDot, dots[i], dots[i + 1], P, check_border);
+			answer = "Belongs polygon";
+			if ( answ == true)
 			{
-				return answer+= "Inside ManyAngle";
+				if (check > 1 || check == 0)
+				{
+					cout << "Inside in polygon";
+					answer = "Inside in polygon";
+				}
+				if (check == 1)
+				{
+					answer = "On border";
+				}
+
+
 			}
 			else
-			{
-				if (answer.compare("\nGiven dot in the triangle") == 0)
+				if (answ == false)
 				{
-					return answer;
+					answer = "Not Inside in polygon";
+					continue;
 				}
-				else
-				{
-					if (answer.compare("\nThe dot is on the border") == 0 && check_border < 2)
-							{
-								check_border++; //если встретили на границе точку
-								continue;
-							}
-							else
-								if (answer.compare("\nGiven dot is not in the triangle") == 0 && check_border == 2)
-								{
-									return answer = "\nGiven dot is on the border of the polygon";
-								}
-								else
-								{
-									if (answer.compare("\nGiven dot is not in the triangle") == 0 && check_border == 1)
-									{
-										return answer = "\nGiven dot is on the outer border previous Trinagle";
-									}
-									else
-									{
-										if (answer.compare("\nGiven dot is not in the triangle") == 0)
-										{
-											continue;
-										}
-									}
-								}
-				}
-				
-			}
+			cout << endl << answer << endl;
+			//	if (answer.compare("\nGiven dot in the triangle") == 0 && check_border == 1)
+			//	{
+			//		return answer+= "Inside ManyAngle";
+			//	}
+			//	else
+			//	{
+			//		if (answer.compare("\nGiven dot in the triangle") == 0)
+			//		{
+			//			return answer;
+			//		}
+			//		else
+			//		{
+			//			if (answer.compare("\nThe dot is on the border") == 0 && check_border < 2)
+			//					{
+			//						check_border++; //если встретили на границе точку
+			//						continue;
+			//					}
+			//					else
+			//						if (answer.compare("\nGiven dot is not in the triangle") == 0 && check_border == 2)
+			//						{
+			//							return answer = "\nGiven dot is on the border of the polygon";
+			//						}
+			//						else
+			//						{
+			//							if (answer.compare("\nGiven dot is not in the triangle") == 0 && check_border == 1)
+			//							{
+			//								return answer = "\nGiven dot is on the outer border previous Trinagle";
+			//							}
+			//							else
+			//							{
+			//								if (answer.compare("\nGiven dot is not in the triangle") == 0)
+			//								{
+			//									continue;
+			//								}
+			//							}
+			//						}
+			//		}
+			//		
+			//	}
+			//}	
 		}
 		return answer;
 	}
@@ -416,57 +397,19 @@ int main()
 	vector<PointF> points;
 	cout << "---------------START--------------------\n";
 	cout << "Enter the coordinates for the following dots:\n"; 
-	//int n = 0;
-	//int  triangle = n - 2;
-	//do
-	//{
-	//	cout << "Enter the number of vertex:\n";
-	//	cin >> n;
-	//} while (n < 2);
-
-	//for (int i = 0; i < n; i++)
-	//{
-	//	cout << "Enter the X and Y:\n";
-	//	cin >> x >> y;
-	//	PointF point(x, y);
-	//	points.push_back(point);
-	//}
 	
 	/*Инициализаци GDI+*/
 	ULONG_PTR gdToken;
 	GdiplusStartupInput gdInput;
 
 	LSTATUS retRes = GdiplusStartup(&gdToken, &gdInput, NULL);	/*Создание главного файла и обработка ошибки */
-
+	// Test
 	PointF A(1.0f, 2.0f);
 	PointF B(2.0f, 4.0f);
 	PointF C(4.0f, 4.5f);
 	PointF D(4.0f, 2.0f);
 	PointF E(3.0f, 1.0f);
 
-	// Test
-	/*A.setCoordinates(1.0,2.0);
-	B.setCoordinates(2.0,4.0);
-	C.setCoordinates(4.0,4.0);
-	D.setCoordinates(4.0,2.0);
-	E.setCoordinates(3.0,1.0);*/
-
-	// Формула для расчета координат середины отрезка: 
-	// XcenterP = (C.X() + A.X())/2
-	// YcenterP = (C.Y() + A.Y())/2
-
-	// TASK: Найти такие координаты точки P, чтобы проверить ее на check_border == 2
-
-	/*float XcenterP = (C.X + A.X) / 2;
-	float YcenterP = (C.Y + A.Y) / 2;*/	
-
-
-	//vector<PointF> dots;
-	//dots.push_back(A);
-	//dots.push_back(B);
-	//dots.push_back(C);
-	//dots.push_back(D);
-	//dots.push_back(E);
 
 	points.push_back(A);
 	points.push_back(B);
@@ -477,22 +420,13 @@ int main()
 	float XcenterP = (points[2].X + points[0].X) / 2;
 	float YcenterP = (points[2].Y + points[0].Y) / 2;
 
-
-	PointF P(XcenterP, YcenterP); //проверяемая точка ();
+	PointF P(XcenterP, YcenterP); //проверяемая точка 
 
 	Method method = Method();
 	cout << "-------------Check by Polygon baricentric method----------------------- \n";
-	//string result = method.Polygon_method(dots, P);
+
 	string result = method.Polygon_method(points, P);
 	cout << result;
-	// cout << "-------------Check by triangle method----------------------- \n";
-	// string result = Triangle_method(A, B, C, P);
-	// cout << "\n";
-	// cout << result;
-
-	// cout << "---------------Check by baricentric method---------------------\n";
-	// string BarResult = Baricenter_method(A, B, C, P);
-	// cout << BarResult;
 
 	GdiplusShutdown(gdToken);
 	return 0;
