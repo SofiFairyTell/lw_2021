51049b66a03b4890150cfb453947038357228f99
Kurbatova <fairytellsblog@gmail.com>
2021-04-06 11:55:42 +0300

    edit output
    - форма вывода исправлена. теперь в выводе пользователь не видит упоминания класса fraction
    - в конце решения выводится был ли это поиск максимума или минимума (берется из первой строки)

---
+++ MMC/main.py
@@ -0,0 +1,9 @@
+from Simplex import Simplex
+objective = ('maximize', '1x_1 + 2x_2')
+constraints = ['4x_1 - 2x_2 <= 12', '-1x_1 + 3x_2 <= 6', '2x_1 + 4x_2 >= 16']
+Lp_system = Simplex(num_vars=2, constraints=constraints, objective_function=objective)
+
+print(Lp_system.solution)
+# {'x_2': Fraction(6, 5), 'x_1': Fraction(3, 5)}
+print("Function Solution: {0} {1}".format(Lp_system.optimize_val, objective[0]))
+# 17/5
