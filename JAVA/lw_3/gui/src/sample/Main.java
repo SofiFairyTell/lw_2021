88578a1efe86813d293586c8ae52d2abb0329c24
Kurbatova <fairytellsblog@gmail.com>
2021-04-07 22:15:04 +0300

    add figure
    - начата работа над добавлением элементов

--- JAVA/lw_3/gui/src/sample/Main.java
+++ JAVA/lw_3/gui/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("lw3oop.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Lw_3");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
