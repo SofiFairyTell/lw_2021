467881f7f96112a7a0dab35a4582ebec9aa5c8d6
Kurbatova <fairytellsblog@gmail.com>
2021-04-10 02:05:34 +0300

    add new prj
    - добавлены материалы нового проекта

---
+++ JAVA/lw_4/src/ru/bstu/it32/kurbatova/lab4/Main.java
@@ -0,0 +1,27 @@
+package ru.bstu.it32.kurbatova.lab4;
+/**
+ * На вход программе подается произвольный html-файл. Необходимо всем тегам <img> добавить недостающие атрибуты
+ * height и width со значениями заданными пользователем.
+ * Пример:
+ * <img src='kartinka.jpg' height='100'/>
+ * <img src='kartinka.jpg' width='100'/>
+ * <img src='kartinka.jpg' />
+ * На выходе:
+ * <img src='kartinka.jpg' height='100' width='200'/>
+ * <img src='kartinka.jpg' height='300' width='100'/>
+ * <img src='kartinka.jpg' height='200' width='300'/>
+ *
+ * 100, 200 и 300 это значения введенные пользователем*/
+public class Main {
+    private void FileReader(String filename)
+    {
+
+    }
+    private void FileParser(String line)
+    {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
