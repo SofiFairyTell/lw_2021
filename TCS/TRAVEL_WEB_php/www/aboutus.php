ddf0b95fc8184af6357995a68738e7effc4511b7
SofiFairyTell <fairytellsblog@gmail.com>
2021-04-22 12:42:25 +0300

    delete
    - удален лишний код

--- TCS/TRAVEL_WEB_php/www/aboutus.php
+++ TCS/TRAVEL_WEB_php/www/aboutus.php
@@ -43,15 +43,7 @@
 <?php
  require('script.php');
 ?>
-<!-- <script>
-	document.addEventListener('DOMContentLoaded', function () {
-	document.body.classList.add('loaded_hiding');
-	window.setTimeout(function () {
-		document.body.classList.add('loaded');
-		document.body.classList.remove('loaded_hiding');
-	}, 2000);
-	});
-</script> -->
+
 
 </body>
 </html>
--- TCS/TRAVEL_WEB_php/www/contacts.php
+++ TCS/TRAVEL_WEB_php/www/contacts.php
@@ -108,16 +108,6 @@
 <?php
  require('script.php');
 ?>
-	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="./js/travelblock.js"></script>
-	<script>
-		document.addEventListener('DOMContentLoaded', function () {
-		document.body.classList.add('loaded_hiding');
-		window.setTimeout(function () {
-			document.body.classList.add('loaded');
-			document.body.classList.remove('loaded_hiding');
-		}, 2000);
-		});
-	</script> -->
+
 </body>
 </html>
