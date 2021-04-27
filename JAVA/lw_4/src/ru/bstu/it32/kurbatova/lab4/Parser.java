9febb313a9558177d4b14d64da98ed0ddd2ecbb1
SofiFairyTell <fairytellsblog@gmail.com>
2021-04-27 13:49:44 +0300

    add pattern
    - если вторая группа пустая, то проверим по другому шаблону
    - по прежнему полная замена 3-й группы

--- JAVA/lw_4/src/ru/bstu/it32/kurbatova/lab4/Parser.java
+++ JAVA/lw_4/src/ru/bstu/it32/kurbatova/lab4/Parser.java
@@ -9,31 +9,58 @@ public class Parser
     public void HtmlParser(String filePath, String replacement) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("result.html"));
-        String patt = "<img.*?src?= ?['\\\"]([^'\\\"]*)['\\\"](.*?)\\/";
+        //String patt = "<img.*?src?= ?['\\\"]([^'\\\"]*)['\\\"](.*?)\\/";
+        String patt = "<img.*?src?= ?['\\\"]([^'\\\"]*)['\\\"](.*?)(width.*?|height.*?|\\S)\\/";
+
+        //String patt2 ="(?|(width.*?['\\\"]([^'\\\"]*)['\\\"])|(height.*?['\\\"]([^'\\\"]*)['\\\"]))";
         //Разбить паттерна три группы! Тогда  первой группе д.б. img + src, потом вставка наших значений и соединение со второй группой
         Pattern pattern = Pattern.compile(patt);
+
         Matcher matcher;
 
         String input = "";
         StringBuilder result = new StringBuilder();
         while ((input = bufferedReader.readLine()) != null) {
             matcher = pattern.matcher(input);
+            int lenght = matcher.groupCount();
+
             if (matcher.find())
             {
-                String group = matcher.group(2);
+
+                       if(matcher.group(2)=="")
+                       {
+                           String pat = "<img.*?src?= ?['\\\"]([^'\\\"]*)['\\\"](.*?)\\/";
+                           Pattern patte = Pattern.compile(pat);
+                           matcher = patte.matcher(input);
+                           if (matcher.find())
+                           {
+                               String group = matcher.group(2) + replacement;
+                               input = input.replaceAll(matcher.group(2),group);
+                               System.out.println(input);
+                               result.append(input).append("\n");
+                           }
+                       }
+                       else
+                       {
+                           String group = matcher.group(2) + replacement;
+                           input = input.replaceAll(matcher.group(2)+matcher.group(3),group);
+                           System.out.println(input);
+                           result.append(input).append("\n");
+                       }
                 //String replacement = " width = '"+width+"' height = '"+height+"'";
-                input = input.replaceAll(matcher.group(2),replacement);
-                System.out.println(input);
-                result.append(input).append("\n");
             }
             else
             {
                 result.append(input).append("\n");
             }
-
         }
         System.out.println(result);
         bufferedWriter.write(result.toString());
         bufferedWriter.close();
+
+
+
+
+
     }
 }
