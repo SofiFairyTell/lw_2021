3c654395135e39056ed1ca4bb093b14ab11b1bdf
SofiFairyTell <fairytellsblog@gmail.com>
2021-05-07 16:33:04 +0300

    add upload with watermark
    - теперь при загрузке изображения на не появляется название сайта
    - код с загрузкой вынесен в отдельный файл

--- TCS/TRAVEL_WEB_php/www/filesizeform.php
+++ TCS/TRAVEL_WEB_php/www/filesizeform.php
@@ -25,82 +25,16 @@ include_once $_SERVER['DOCUMENT_ROOT'] . './scriptphp/watermark.php';
 </div>
 </section>
 <section class = "section">
-<div>
+
     <span> Фотография с Watermark</span>
-   <?php 
-
-    $watermark = new Watermark();
-    $img = imagecreatefromjpeg('img/photo3.jpg');
-
-    $font = dirname(__FILE__) . '/fonts/verdana.ttf';;
-    $image = $watermark->add_watermark($img,'travelblog.ru',$font);
- 
-    //выводим изображение
-    imageJPEG($img,'img/simpletext.jpg');
-
-   //imagejpeg($image, NULL, 75);
-    //освобождаем память
-    imagedestroy($image);
-   // imagedestroy($img);
-    ?>
-</div>
-</section>
-
-<section class = "section">
-<form enctype="multipart/form-data" action="filesizeform.php" method="post">
+    <form enctype="multipart/form-data" action="filesizeform.php" method="post">
 			Выберите файл: <input name="uploadedFile" type="file" />
 			<input type="submit" name = "uploadBtn" value="Загрузить" />
 		</form>
 <?php
-$message = ''; 
-if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Загрузить')
-{
-  if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
-  {
-    // get details of the uploaded file
-    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
-    $fileName = $_FILES['uploadedFile']['name'];
-    $fileSize = $_FILES['uploadedFile']['size'];
-    $fileType = $_FILES['uploadedFile']['type'];
-    $fileNameCmps = explode(".", $fileName);
-    $fileExtension = strtolower(end($fileNameCmps));
-
-    // sanitize file-name
-    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
-
-    // check if file has one of the following extensions
-    $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
-
-    if (in_array($fileExtension, $allowedfileExtensions))
-    {
-      // directory in which the uploaded file will be moved
-      $uploadFileDir = './uploaded_files/';
-      $dest_path = $uploadFileDir . $newFileName;
-
-      if(move_uploaded_file($fileTmpPath, $dest_path)) 
-      {
-        $message ='File is successfully uploaded.';
-      }
-      else 
-      {
-        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
-      }
-    }
-    else
-    {
-      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
-    }
-  }
-  else
-  {
-    $message = 'There is some error in the file upload. Please check the following error.<br>';
-    $message .= 'Error:' . $_FILES['uploadedFile']['error'];
-  }
-}
-?>        
+ require('upload.php');
+ ?>      
 </section>
-
-
 
 </body>
 </html>
---
+++ TCS/TRAVEL_WEB_php/www/upload.php
@@ -0,0 +1,56 @@
+<?php
+$message = ''; 
+if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Загрузить')
+{
+  if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+  {
+    // get details of the uploaded file
+    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+    $fileName = $_FILES['uploadedFile']['name'];
+    $fileSize = $_FILES['uploadedFile']['size'];
+    $fileType = $_FILES['uploadedFile']['type'];
+  
+//добавим надпись к загружаемому изображению
+    $watermark2 = new Watermark();
+    $img = imagecreatefromjpeg($fileTmpPath);
+    $font = dirname(__FILE__) . '/fonts/verdana.ttf';;
+    $image = $watermark2->add_watermark($img,'travelblog.ru',$font);     
+    imageJPEG($img,$fileTmpPath);//выводим изображение
+    imagedestroy($image);//освобождаем память
+
+    $fileNameCmps = explode(".", $fileName);
+    $fileExtension = strtolower(end($fileNameCmps));
+
+    // sanitize file-name
+    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+
+    // check if file has one of the following extensions
+    $allowedfileExtensions = array('jpg', 'gif', 'png');
+
+    if (in_array($fileExtension, $allowedfileExtensions))
+    {
+      // directory in which the uploaded file will be moved
+      $uploadFileDir = './uploaded_files/';
+      $dest_path = $uploadFileDir . $newFileName;
+
+      if(move_uploaded_file($fileTmpPath, $dest_path)) 
+      {
+        $message ='File is successfully uploaded.';
+      }
+      else 
+      {
+        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+      }
+    }
+    else
+    {
+      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+    }
+  }
+  else
+  {
+    $message = 'There is some error in the file upload. Please check the following error.<br>';
+    $message .= 'Error:' . $_FILES['uploadedFile']['error'];
+  }
+}
+?>       
