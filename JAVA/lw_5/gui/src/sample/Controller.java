316753a3b56ab5366ca9be8788dea922945dea78
SofiFairyTell <fairytellsblog@gmail.com>
2021-05-09 00:35:30 +0300

    change interface
    - внесены изменения в внешний вид приложения

--- JAVA/lw_5/gui/src/sample/Controller.java
+++ JAVA/lw_5/gui/src/sample/Controller.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.*;
+import java.io.IOException;
 
 public class Controller
 {
