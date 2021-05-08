e05967984554ceea783edcced9defc39ab081c06
SofiFairyTell <fairytellsblog@gmail.com>
2021-05-08 22:13:55 +0300

    first version jvafx-5
    - первая версия проекта по JAVA lw_5

---
+++ JAVA/lw_5/gui/src/sample/Controller.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+
+public class Controller
+{
+    @FXML
+    private TextField textParam;
+    @FXML
+    private TextArea htmlEdit;
+    @FXML
+    private TextArea htmlEdit1;
+    @FXML
+    private Button btn;
+@FXML
+    public void EditHtml(javafx.event.ActionEvent actionEvent) throws IOException {
+
+    }
+
+}
+
