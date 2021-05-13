b9079eac76cb6cae1b9e7c11977caf7c9104285f
SofiFairyTell <fairytellsblog@gmail.com>
2021-05-13 20:25:28 +0300

    summary update
    - MMC - добавлен материал для 3-й лабораторной работы
    - JAVA - исправлен интерфейс. дата через DatePicker

--- JAVA/lw_5/gui/src/sample/Controller.java
+++ JAVA/lw_5/gui/src/sample/Controller.java
@@ -2,11 +2,14 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import ru.bstu.it32.kurbatova.lab5.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Controller
 {
@@ -15,6 +18,10 @@ public class Controller
 
     @FXML
     private TextField event_name_TA;
+    @FXML
+    private DatePicker date_start_TA_pick;
+    @FXML
+    private DatePicker date_end_TA_pick;
     @FXML
     private TextField date_start_TA;
     @FXML
@@ -65,8 +72,15 @@ public class Controller
     event_name_TA.setText("Встреча");
         var event_name = event_name_TA.getText();
         var event_type = event_type_CB.getSelectionModel().getSelectedItem().toString();
-        var date_start =  date_start_TA.getText();
-        var date_end = date_end_TA.getText();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+        LocalDate date_start_ta_pickValue = date_start_TA_pick.getValue();
+        LocalDate date_end_ta_pickValue = date_end_TA_pick.getValue();
+                
+        var date_start =  date_start_ta_pickValue.format(formatter);
+        var date_end = date_end_ta_pickValue.format(formatter);
+
+
         var manager =  manager_TA.getText();
         var place = place_TA.getText().trim();
         var searchId = Integer.parseInt(id_TA.getText().trim());
